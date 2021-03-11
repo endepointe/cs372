3f01b5bc7462a3a3af0f4e4c1b2df73dc2108d00
EndePointe <endepointe@gmail.com>
2021-03-11 17:18:14 +0000

    client and server close on q signal

--- chat_client.py
+++ chat_client.py
@@ -6,19 +6,37 @@ PORT = int(sys.argv[1])
 buffer = 4096
 
 if __name__ == "__main__":
+  # 1. the client creates a socket
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as chat_client:
-    chat_client.connect((HOST,PORT))
-    client_msg = ''
-    connected = 1
-    while connected:
-      client_msg = input("Enter message: ")
-      if "/q" == client_msg:
-        connected = 0
-        break
-      chat_client.sendall(bytes(str(client_msg), encoding="utf-8"))
-      server_res = chat_client.recv(buffer)
-      if "/q" == server_res.decode("utf-8"):
-        connected = 0
-        break
-      print("->SERVER: ", server_res.decode("utf-8")) 
-    chat_client.close()
+    try:
+      # 1. connects to localhost and port
+      chat_client.connect((HOST,PORT))
+      client_msg = ''
+      connected = 1
+      while connected:
+        # 2. when connected, the client prompts for a message to send
+        client_msg = input("Enter message: ")
+        # 3. if the messge is /q, the client quits
+        if "/q" == client_msg:
+          connected = 0
+          break
+        # 4. otherwise, the client sends the message
+        chat_client.sendall(bytes(str(client_msg), encoding="utf-8"))
+        # 5. the client calls recv to receive data
+        server_res = chat_client.recv(buffer)
+        if not server_res: 
+          connected = 0
+          chat_client.close()
+          break
+        if server_res.decode("utf-8") == "/q":
+          connected = 0
+          chat_client.close()
+          break
+        # 6. the client prints the data
+        print("->SERVER: ", server_res.decode("utf-8")) 
+        # 7. back to step 2
+    except ConnectionAbortedError:
+      print("SERVER DISCONNECTED")
+      chat_client.close()
+    # 8. sockets are closed, using with
+    #chat_client.close()
--- chat_server.py
+++ chat_server.py
@@ -7,24 +7,35 @@ buffer = 4096
 
 if __name__ == "__main__":
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as chat_server:
-    # the microserver location that all other services will connect to
-    chat_server.bind((HOST, PORT))
-    # enable a server to accept connections
-    chat_server.listen(5)
-    connected = 1
-    #while connected: # run the server until told to disconnect.
-    (chat_client, addr) = chat_server.accept()
-    with chat_client: 
+    try:
+      # 1. The server creates a socket and binds to localhost and port
+      chat_server.bind((HOST, PORT))
+      # 2. the server listens for a connection
+      chat_server.listen(5)
+      connected = True
+      # 3. when connected, the server calls recv to receive data
+      (chat_client, addr) = chat_server.accept()
+      #with chat_client: 
       server_msg = '' 
       while connected:
+        # 3. continued
         client_res = chat_client.recv(buffer)
-        print("->CLIENT: ", client_res.decode("utf-8"))
-        # prompt for a reply
-        server_msg = input("Enter Message: ")
-        if "\q" == server_msg:
-          connected = 0
-          chat_client.sendall(bytes(str(server_msg), encoding="utf-8"))
+        if not client_res: 
+          connected = False
           break
+        # 4. the server prints the data
+        print("->CLIENT: ", client_res.decode("utf-8"))
+        # 4. the prompts for a reply
+        server_msg = input("Enter Message: ")
+        # 5. if the reply is /q, the server quits
+        if "\q" == server_msg:
+          connected = False
+          break
+        # 6 otherwise, the server sends the reply
         chat_client.sendall(bytes(str(server_msg), encoding="utf-8"))
+        # 7 back to step 3 
+    except ConnectionAbortedError:
+      print("CLIENT DISCONNECTED")
       chat_client.close()
-      chat_server.close()
+  chat_server.close()
+    # 8. sockets are closed, using with
